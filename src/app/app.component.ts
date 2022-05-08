ede64912600936ce081329064997674cd91b80e4
Manuel Moreira <manuel.moreira.valle@gmail.com>
2022-05-07 21:22:53 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'testServer', content: 'Just a test!'}];
+
+}
